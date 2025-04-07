5203bc0d1eb7dbf18b469182000c7cee4a25bae6
informatica-cn <informatica@cerronavia.cl>
2025-04-07 16:41:05 -0400

    configuracion inicial

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+
+import { Text, View } from 'react-native'
+
+const CalculatorApp = () => {
+    return
+    (
+        <View>
+            <Text>CalculatorApp</Text>
+        </View>
+    )
+}
+
+export default CalculatorApp
